e30dc0b9db1859965db796987c6aee33dfe0a677
primedevintern10 <primedevintern10@gmail.com>
2026-03-08 19:11:36 +0530

    Refactor database connection and logging setup; move to dedicated config files

--- config/logging_config.py
+++ config/logging_config.py
@@ -1,14 +1,18 @@
 # logging_config.py
+import sys
 import logging
 
 
 def setup_logging():
+    if hasattr(sys.stdout, "reconfigure"):
+        sys.stdout.reconfigure(encoding="utf-8")
+
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         handlers=[
             logging.StreamHandler(),
-            logging.FileHandler("config/app.log")
+            logging.FileHandler("config/app.log", encoding="utf-8")
         ]
     )
     logging.getLogger("uvicorn.access").setLevel(logging.WARNING)
